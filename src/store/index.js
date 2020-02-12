90942c1ced2a05de977fdc87e6ceeef7b45b2f44
stas <voznyuk_stas@mail.ru>
2020-02-12 18:40:54 +0300

    Set up store

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,13 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
+import post from './modules/post';
+import user from './modules/user';
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
   modules: {
+    post,
+    user
   }
 })
